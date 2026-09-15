73a506991939649f638c2b32c5574cc20a698cef
jagadishjk1 <jagadishjk1998@gmail.com>
2026-09-15 23:22:50 +0530

    list-prac

---
+++ lists/sortingPrac.py
@@ -0,0 +1,14 @@
+# Create a list of 6 unsorted numbers. Print it. Then:
+un_sorted = [34, 89, 39, 67, 91, 49]
+
+# Sort it in ascending order using .sort(), print it
+un_sorted.sort()
+print(un_sorted)
+
+# Sort it in descending order, print it
+un_sorted.sort(reverse=True)
+print(un_sorted)
+
+# Use .reverse() on the descending list and print the result — think about what order you expect before running it
+un_sorted.reverse()
+print(un_sorted)
