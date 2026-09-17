826736dc46b2da5972438a5a452e3a2907f9b7c2
jagadishjk1 <jagadishjk1998@gmail.com>
2026-09-17 12:11:49 +0530

    loops-prac2

--- loops/stringloop.py
+++ loops/stringloop.py
@@ -1,4 +1,12 @@
 # Ask the user for a sentence. Loop through each character in the string.
 #count how many vowels (a, e, i, o, u — case-insensitive) it contains.
 
-# Start with Task 1 — paste your code when ready.
+sentence = input("Enter a letter/sentence you like: ")
+case_sentence = sentence.lower()
+print(case_sentence)
+total_vowels = 0
+for letter in case_sentence:
+    if letter in ["a", "e", "i", "o", "u"]:
+        total_vowels += 1
+
+print(f"Total vowels in given sentence/word: {total_vowels}")
